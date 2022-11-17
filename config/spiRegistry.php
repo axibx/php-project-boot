38384cf5aefc7355caca26993d67a8eb374e4c79
junwei.ye <junwei.ye@weimob.com>
2022-11-17 16:07:54 +0800

    read style adjust

--- config/spiRegistry.php
+++
@@ -1,7 +0,0 @@
-<?php
-/** @var WeimobCloudBoot\Ability\Spi\SpiRegistry $spiRegistry */
-
-use WeimobCloudBoot\Ability\SpecTypeEnum;
-
-$spiRegistry->register("demoSpiImpl",\WeimobCloudBoot\Ability\Spi\DemoSpiImpl::class,SpecTypeEnum::WOS);
-$spiRegistry->register("demoSpiImpl",\WeimobCloudBoot\Ability\Spi\DemoXinyunSpiImpl::class,SpecTypeEnum::XINYUN);
